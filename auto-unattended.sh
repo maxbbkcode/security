bc5742e1f22bea7a658deb6ff0146b54a3412ba4
maxbbkcode <maxbbk.code@mail.ru>
2026-03-12 09:50:51 +0300

    Refactor auto-unattended.sh for writing config
    Refactor script to write auto-upgrade settings using echo commands instead of here-document.

--- auto-unattended.sh
+++ auto-unattended.sh
@@ -14,10 +14,9 @@ sed -i 's|^[[:space:]]*"\${distro_id}ESMApps:\${distro_codename}-apps-security";
 sed -i 's|^[[:space:]]*"\${distro_id}ESM:\${distro_codename}-infra-security";|// "${distro_id}ESM:${distro_codename}-infra-security";|' "$FILE"
 
 echo "Writing /etc/apt/apt.conf.d/20auto-upgrades ..."
-cat > /etc/apt/apt.conf.d/20auto-upgrades <<'EOF'
-APT::Periodic::Update-Package-Lists "1";
-APT::Periodic::Unattended-Upgrade "1";
-EOF
+
+echo 'APT::Periodic::Update-Package-Lists "1";' > /etc/apt/apt.conf.d/20auto-upgrades
+echo 'APT::Periodic::Unattended-Upgrade "1";' >> /etc/apt/apt.conf.d/20auto-upgrades
 
 echo
 echo "Current /etc/apt/apt.conf.d/20auto-upgrades:"
@@ -25,6 +24,7 @@ cat /etc/apt/apt.conf.d/20auto-upgrades
 
 echo
 echo "unattended-upgrades status:"
+systemctl restart unattended-upgrades
 systemctl status unattended-upgrades --no-pager || true
 
 echo
